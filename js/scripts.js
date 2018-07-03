0294fc9642f6c362105a95155f09561b5bbc266c
Andi Lindblom <andilindblom@gmail.com>
2018-07-02 20:37:06 -0700

    day 1 in class notes and practice

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//DECLARE A VARIABLE
+var firstName;
+
+//camelCasing:
+
+//DEFINE YOUR VARIABLE
+firstName = "Andi"; //anything in quotes in js is called a String
+
+//DECLARE AND DEFINE YOUR VARIABLE (shorthand)
+var lastName = "Lindblom";
+
+console.log(firstName, lastName);
+
+/* you can redefine a variable later in your code
+by typing variable name = "new definition" */
+
+// SINGLE LINE COMMENT
+
+/* MULTILINE COMMENT
+firstName = "Superman";
+console.log(firstName, lastName)
+*/
+
+/* **MAKE SURE variable names are specific and unique. makes code easy to read!
+also comment what the variable is going to do! very helpful! */
+
+{
+	//New Scope.
+	var myName; //single space tab inside curly brackets when you start new scope.
+}
+
+// JS statements should always be terminated with a semicolon
