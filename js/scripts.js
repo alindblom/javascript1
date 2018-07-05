9ea1b12b6659762a7a11115553bf720a421e467f
Andi Lindblom <andilindblom@gmail.com>
2018-07-04 22:55:47 -0700

    day 1 takehome assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,5 @@
-//DECLARE A VARIABLE
-var firstName;
+var classGoal = "I'm hoping to learn enough javascript to make my websites look fancier than they are currently.";
 
-//camelCasing:
+var favoriteHobby = "My favorite hobby is drawing, specifically characters and character design, and playing video games.";
 
-//DEFINE YOUR VARIABLE
-firstName = "Andi"; //anything in quotes in js is called a String
-
-//DECLARE AND DEFINE YOUR VARIABLE (shorthand)
-var lastName = "Lindblom";
-
-console.log(firstName, lastName);
-
-/* you can redefine a variable later in your code
-by typing variable name = "new definition" */
-
-// SINGLE LINE COMMENT
-
-/* MULTILINE COMMENT
-firstName = "Superman";
-console.log(firstName, lastName)
-*/
-
-/* **MAKE SURE variable names are specific and unique. makes code easy to read!
-also comment what the variable is going to do! very helpful! */
-
-{
-	//New Scope.
-	var myName; //single space tab inside curly brackets when you start new scope.
-}
-
-// JS statements should always be terminated with a semicolon
+console.log(classGoal, favoriteHobby);
