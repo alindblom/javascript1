3e02855c8d27d72842ad167f3ff42f132351d7ca
Andi Lindblom <andilindblom@gmail.com>
2018-07-16 20:00:30 -0700

    notes for day4: arrays and loops

--- js/scripts.js
+++ js/scripts.js
@@ -110,41 +110,156 @@ console.log(Array.isArray(myFavs), typeof myFavs);
 	// myFavs.splice(removeIndex, 1); //THIS IS USED OFTEN!!!
 
 //Reverse example
-	let myNums = [1, 2, 3, 4, 5];
-	console.log(myNums.reverse());
-
-	//Sort example
-	console.log(myFavs);
-	myFavs.sort();
-	console.log(myFavs);
-
-	// NESTED ARRAYS
-
-	let multiDimensional = [['hello', 1], ['goodbye', 0]];
-
-	console.log(multiDimensional[0][0], multiDimensional[1][0]);
-	// the first [] is the parent array, the second [] is the arrays inside the parent, etc...
-
-	//TRY IT OUT #2
-	//1.
-	let firstMovie = ['Ant-Man', 'Scott Lang'];
-	let secondMovie = ['Birdcage', 'Armond'];
-	let thirdMovie = ['Atlantis', 'Milo'];
-	let fourthMovie = ['Captain America: Winter Soldier', 'Steve Rogers'];
-	let fifthMovie = ['Thor Ragnarok', 'Thor'];
-	let faveMovies = [];
-
-	faveMovies.push(firstMovie, secondMovie, thirdMovie, fourthMovie, fifthMovie);
-	console.log(faveMovies);
-	//2.
-	let movieTitles = [];
-	movieTitles.push(faveMovies[0][0], faveMovies[1][0], faveMovies[2][0], faveMovies[3][0], faveMovies[4][0]);
-	alert(movieTitles);
-	//3.
-	let favAndLeast = [];
-	favAndLeast.push(faveMovies[0], faveMovies[1]);
-	console.log(favAndLeast);
-	//4.
-	alert(faveMovies.length);
+		// let myNums = [1, 2, 3, 4, 5];
+		// console.log(myNums.reverse());
+		//
+		// //Sort example
+		// console.log(myFavs);
+		// myFavs.sort();
+		// console.log(myFavs);
+		//
+		// // NESTED ARRAYS
+		//
+		// let multiDimensional = [['hello', 1], ['goodbye', 0]];
+		//
+		// console.log(multiDimensional[0][0], multiDimensional[1][0]);
+		// // the first [] is the parent array, the second [] is the arrays inside the parent, etc...
+		//
+		// //TRY IT OUT #2
+		// //1.
+		// let firstMovie = ['Ant-Man', 'Scott Lang'];
+		// let secondMovie = ['Birdcage', 'Armond'];
+		// let thirdMovie = ['Atlantis', 'Milo'];
+		// let fourthMovie = ['Captain America: Winter Soldier', 'Steve Rogers'];
+		// let fifthMovie = ['Thor Ragnarok', 'Thor'];
+		// let faveMovies = [];
+		//
+		// faveMovies.push(firstMovie, secondMovie, thirdMovie, fourthMovie, fifthMovie);
+		// console.log(faveMovies);
+		// //2.
+		// let movieTitles = [];
+		// movieTitles.push(faveMovies[0][0], faveMovies[1][0], faveMovies[2][0], faveMovies[3][0], faveMovies[4][0]);
+		// alert(movieTitles);
+		// //3.
+		// let favAndLeast = [];
+		// favAndLeast.push(faveMovies[0], faveMovies[1]);
+		// console.log(favAndLeast);
+		// //4.
+		// alert(faveMovies.length);
 
 // LOOPS
+for (let i = 0; i < 3; i++) {
+	console.log('Hi!');
+}
+/*let i = 0 -> declaring variable named i and setting starting value to 0
+	i < 3 -> confision required to execute block of code. ie: if i is less than 3, the code executes.
+	i++ -> modifier to variable i. changes value of i AFTER block of code runs and starts loop all over again.
+
+	- TO END LOOP PREMATURELY ->
+		- break; -> once it hits break, it exits out of the loop
+	- TO SKIP PART OF CODE IN LOOP ->
+		- continue; -> halts running everything in block of code for that loop, and jumps back to the for loop conditions (starts over loop)
+		 ** good for skipping an item in an array while still executing the rest of the code
+//PITFALLS
+for (let i = 0; i > 5; i++) {
+	console.log('Hi!');
+}
+  - code just won't run since condition is false.
+
+for (let i = 0; i (>)=//0; i++) {
+	console.log('Hi!');
+}
+	- code is an infinite loop. DO NOT RUN THIS!!!!
+
+** usually only use i++ or i--
+** DON'T MODIFY i IN THE BLOCK OF CODE
+
+*/
+	// for (let i = 0; i < 3; i++) {
+	// 	console.log(`The number is ${i}`);
+	// }
+//console logs 3 lines `The number is 0`, etc.
+
+	// for (let i = 0; i < 3; i++) {
+	// 	console.log(`The number is ${i + 10}`);
+	// }
+//can manipulate i in {} instead of changing it completely
+
+//EXAMPLE
+// for (let i = 0; i < 11; i++) {
+// 	console.log(`The number is ${i * 10}`);
+// }
+// console logs multiples of 10 to 100
+
+//EXAMPLE: console logging each item of the array
+	// const colors = ['blue', 'purple', 'green', 'red', 'aquamarine', 'chartreuse'];
+	// for (let index = 0; index < colors.length; index++) {
+	// 	console.log(colors[index]);
+	// }
+	// //index < colors.length will run code until you reach the end of an array
+
+// /* WHILE LOOPS
+	// 	- as soon as condition is done, while loop will stop and not read whatever else is in the block
+	// 	- pretty much does the same thing as a for loop.
+	// */
+	// let counter = 0;
+	// while (counter < 5) {
+	// 	console.log(`before ${counter}`);
+	// 	counter++; //NEED THIS PART!!! otherwise it will run forever
+	// 	console.log(`after ${counter}`);
+	// }
+
+// LOOPS MADE FOR ARRAYS
+
+/* FOR EACH -> when you just want the values and/or index of the array, use forEach
+	- forEach will give its variables a value automatically
+		forEach (first, second):
+		* first variable = value of array
+		* second variable = index of array
+	** you can name the variables anything.
+*/
+	//EXAMPLE:
+	// const captains = [ 'Kirk', 'Picard', 'Sisko', 'Janeway', 'Lorca' ];
+	// captains.forEach( (captain, index) => {
+	// 	console.log(captain, index);
+	// } );
+
+/* FOR...OF is for ARRAYS
+	- if you're only interested in VALUE of an array, use for...of
+	- used in Angular a lot
+	- for (let variable(value of everything in the array) of [array variable])
+
+*/
+	//EXAMPLE
+	// const captains = [ 'Kirk', 'Picard', 'Sisko', 'Janeway', 'Lorca' ];
+	// for (let captain of captains) {
+	// 	console.log(`Hello, Captain ${captain}.`);
+	//captains.indexOf(captain); //-> how to find index while using for of (just use for each)
+	// }
+
+/*FOR...IN is for OBJECTS
+	- look this up later lmao
+*/
+	// const me = {
+	// 	firstName: 'Andi',
+	// 	lastName: 'Lindblom',
+	// 	age: 24
+	// } //this is an object
+	//
+	// //for (let variable (value of stuff in object) in {object name})
+	// for (let property in me) {
+	// 	console.log(me[property]);
+	// }
+
+//TRY IT OUT: LOOPS
+let firstMovie = ['Ant-Man', 'Scott Lang'];
+let secondMovie = ['Birdcage', 'Armond'];
+let thirdMovie = ['Atlantis', 'Milo'];
+let fourthMovie = ['Captain America: Winter Soldier', 'Steve Rogers'];
+let fifthMovie = ['Thor Ragnarok', 'Thor'];
+let faveMovies = [];
+
+faveMovies.push(firstMovie, secondMovie, thirdMovie, fourthMovie, fifthMovie);
+for (i = 0; i < faveMovies.length; i++) {
+	if(faveMovies[i][0] === "Ant-Man") alert(`${faveMovies[i][0]} ${faveMovies[i][1]}`);
+}
