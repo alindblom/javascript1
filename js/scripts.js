02d4239f3fca7456fd757fd1356c1b54afaaacb8
Andi Lindblom <andilindblom@gmail.com>
2018-07-23 17:59:47 -0700

    takehome stuff

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,11 @@ let userTemp = parseInt(prompt(`What temperature do you want to convert?`));
 function convertTemp(tempScale, userTemp) {
 	if(tempScale = "celcius") {
 		let fTemp = userTemp * 1.8 + 32;
-		return document.querySelector("h3_temp") = fTemp;
+
 	} else if(tempScale = "fahrenheit") {
 		let cTemp = (userTemp - 32) * .5556;
-		return document.querySelector("h3_temp") = cTemp;
+
 	} else {
-		alert("that's not it!")
+		alert
 	}
 }
-
-convertTemp();
