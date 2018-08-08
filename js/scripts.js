70789f8e047498578d605dc44204206bd7e27b4f
Andi Lindblom <andilindblom@gmail.com>
2018-08-07 20:08:44 -0700

    day 11 notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,110 @@
-/* OBJECTS
-- classical inheritance: instead of objects, other programming languages have classes.
-class functions similarly to an object (defines name, method, etc). if you mess up in
-the code anywhere in the heritance chain, it breaks your whole code.
+/* JSON
+- javascript object notation
+- NOT JAVASCRIPT
+- JSON.parse() -> turns raw JSON data into data we can deal with.
+- JSON.stringify() -> turns data into a string (so we can read it!)
+- JSON object =/= javascript object
+- to use JSON objects received from server, you have to convert it into javascript */
 
-- prototypal inheritance: uses objects
-	-> the children inherit everything the parents has. if you mess up, only the child will
-	either break, show undefined, and js keeps reading the code.
+	// let myObject = {
+	// 	name: "Andi",
+	// 	age: NaN
+	// };
+	//
+	// myObject = JSON.stringify(myObject);
+	// console.log(myObject);
+	// // console.logs -> {"name":"Andi","age":null}
+	//
+	// //check out cloned repo for JSON examples ->
+	//
+	// /*PROMISES
+	// - way to send and received information/data asynchronously
+	// - takes in two METHODS (not arguments) {resolve, reject}
+	// 	* if things are good (good response) -> use resolve
+	// 	* if there's an error (bad response) -> use reject
+	// - you can plan on what the error is and take appropriate action.
+	// - promise will return a promise
+	// */
+	//
+	// // define promise
+	// const weCool = false;
+	// const myPromise = new Promise( (resolve, reject) => {
+	// 	if (weCool) resolve('We are in fact, cool.');
+	// 	else if(!weCool) reject('Sorry, we aint cool...');
+	// });
+	// //checking to see if value of weCool resolves to be true.
+	// //if weCool is false -> use reject
+	//
+	// // consume promise -> can't call it like a function bc it's not a function
+	// myPromise.then(result => {
+	// 	console.log(result);
+	// }).catch(error => {
+	// 	console.log(error);
+	// });
+	// // use .then(result => {}) to catch resolve part of a promise
+	// //when false, console log -> Uncaught (in promise) Sorry, we aint cool...
+	// //this is bc result ONLY takes in resolve result
+	// //so bc of this, use .catch(error => {}) for the reject part of a promise
+	//
+	// //function that returns 2
+	// function returnTwo() {
+	// 	return 2;
+	// }
+	// //if you put it into a promise, it will return a promise (check console log)
+	// console.log(Promise.resolve(returnTwo()));
+
+//FETCH API
+/*
+- new to ES6
+- request -> stuff you send out
+- response -> stuff you pull in
 
 */
 
-	// //PROTOTYPE INHERITANCE
-	// let sentence = new String("Ann went walking one day."); //(string type object)
-	// let number = new Number(32); //(number type object)
-	// // console.log(number);
-	//
-	// /* _proto_ = prototype. the prototype of string is object.
-	// Object.getPrototypeOf ->
-	// */
-	//
-	// let dude = { // most basic way to create an object (object type object)
-	// 	catchPhrase: "Where's my car?",
-	// 	firstName: "Big",
-	// 	lastName: "Lebowski",
-	// 	age: Infinity
-	// };
-	// 	// console.log(Object.getPrototypeOf(dude)); //OBJECT
-	//
-	// //Objects are made up of KEY : VALUE pair, which makes a PROPERTY
+//GET REQUEST -> only getting data from server, not sending anything back
+let starTrekSeries;
 
-/*CONSTRUCTOR FUNCTIONS
-- how to make your own new Number(), new String(), etc
-- RULE 1: first letter of constructor function should be capitalized
-- RULE 2: arguments that go into function are going to be spit out as property
-of that function (?)
-
-1.
-
-you can make your own prototypes. end line of prototype chain is object. */
-
-function User(name, email, password, favorites, cart) {
-	this.name = name;
-	this.email = email;
-	this.password = password;
-	this.favorites = favorites;
-	this.cart = cart;
-}; //4th way to bind 'this'
-
-	let cody = new User("Cody", "code-man-5000@gmail.com", "123456", [], []);
-	// always have to call new to build new object with these objects
-	// console.log(cody);
-	// console.log(Object.getPrototypeOf(cody));
-	let andi = new User("Andi", "a@gmail.com", "12323", [], []);
-	andi.isAdmin = true;
-	console.log(andi);
-
-	//creating own method for own object
-	User.prototype.calculateCartTotal = function() {
-		let total = 0;
-		this.card.forEach(item => {
-			total += item.price;
-		});
-		return total;
+//this returns a promise -> so use .then()
+fetch('http://stapi.co/api/v1/rest/series/search').then( response => {
+	// console.log(response);
+	if(response.ok){ //looking at status object of response object {
+		console.log("yay!");
 	}
-	//use dot notation to add new property = new value
+	return response.json(); //returning response, turned it into json format, pass into .then()
+}).then(jsonResult => {
+	console.log(jsonResult.series);
+	starTrekSeries = jsonResult.series; //got array series from api and assigned it to variable
+}).catch( (err) => { console.log(err); }); //to catch errors and be SMART ABOUT IT
+//console.logs response object
+// status -> 3 digit status code (ie. 200 is a good code :) )
+// ok -> if anything comes back worthy of an error, ok is FALSE (good catch all for errors)
+// series -> databases way of holding data
+// **this allows you to take in data and do smth with it (like to put it on the screen)**
 
-//BRACKET NOTATION: different way to refer to properties on an object
-	// andi["42"] = false //bracket notation
-	//if you try to add a number to a key, js will reject it. so you can use bracket notation
+//POST REQUEST -> sending data back to server for server to process
+/*
+mode: 'cors' -> cross origin request (?)
+headers: {
+	'Content-Type': 'application/json' -> the type of data we're sending to server
+}
+body: {
+	number: 555,
+	faveColor: blue -> body is the actual data that you're sending
+}
 
-//TRY IT OUT #1: build a new user with data collected from a form in HTML and your user Prototype
+use .then since you're sending a promise
+*/
 
-//3 inputs from user, build new user based on those 3 inputs
-const userForm = document.forms.userForm;
+//POSTMAN -> app to download
 
-userForm.addEventListener("submit", e => {
-	e.preventDefault();
-	alert("stuffs")
-	let newUser = new User(userForm.name.value, userForm.email.value, userForm.password.value);
-	console.log(newUser);
-	userForm.reset();
-});
+//CLOSURE -> look it up
 
-//
+//ES6 Classes
+// - special function -> does NOT get hoisted
+// - contains special method named constructor - inherently there if it's not written out
+// - can be extended to be declared as a child of a class
+// - classes return an object
+// class and prototype can be interchangable
+
+// syntactic sugar
+//@Component -> @ is a decorator that Angular adds for you
